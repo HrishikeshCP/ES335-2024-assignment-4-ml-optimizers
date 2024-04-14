a0ba0d408dd619353bd5661639bfcc1ace94d30e
Rahul-R-Pai <74908103+Rahul-R-Pai@users.noreply.github.com>
2024-04-14 21:53:12 +0530

    dataset

--- q1/makedata.py
+++ q1/makedata.py
@@ -2,8 +2,8 @@
 from os import makedirs
 from os import listdir
 from shutil import copyfile
-from random import seed
-from random import random
+from random import shuffle
+
 # create directories
 dataset_home = 'q1/monkey_vs_rabbit_dataset/'
 subdirs = ['train/', 'test/']
@@ -13,21 +13,34 @@ for subdir in subdirs:
 	for labldir in labeldirs:
 		newdir = dataset_home + subdir + labldir
 		makedirs(newdir, exist_ok=True)
-# seed random number generator
-seed(1)
-# define ratio of pictures to use for validation
-val_ratio = 0.25
+
 # copy training dataset images into subdirectories
 src_directory = 'q1/dataset/'
+
+# Define the exact number of images for each set
+num_train = 80
+num_test = 20
+
+# Counter variables for each class
+monkey_train_count = 0
+rabbit_train_count = 0
+
+# Shuffle the list randomly
+shuffle(listdir(src_directory))
+
+# Iterate through the files and copy them to appropriate directories
 for file in listdir(src_directory):
-	src = src_directory + '/' + file
-	dst_dir = 'train/'
-	print(file)
-	if random() < val_ratio:
-		dst_dir = 'test/'
-	if file.startswith('monkey'):
-		dst = dataset_home + dst_dir + 'monkey/'  + file
-		copyfile(src, dst)
-	elif file.startswith('rabbit'):
-		dst = dataset_home + dst_dir + 'rabbit/'  + file
-		copyfile(src, dst)
+    src = src_directory + '/' + file
+    if file.startswith('monkey'):
+        if monkey_train_count < num_train:
+            dst = dataset_home + 'train/monkey/' + file
+            monkey_train_count += 1
+        else:
+            dst = dataset_home + 'test/monkey/' + file
+    elif file.startswith('rabbit'):
+        if rabbit_train_count < num_train:
+            dst = dataset_home + 'train/rabbit/' + file
+            rabbit_train_count += 1
+        else:
+            dst = dataset_home + 'test/rabbit/' + file
+    copyfile(src, dst)
